755111762e13e51bc47b3c440910d2ba31bb1ca3
Chudi Huang <t-chhuan@microsoft.com>
2017-04-25 17:00:24 +0800

    token service

---
+++ user_token.js
@@ -0,0 +1,16 @@
+﻿const uuid = require('node-uuid');
+
+var token_database = { 'test': true };
+
+module.exports.validate_token = function (token) {
+    return (token_database[token] != undefined);
+}
+
+module.exports.generate_token = function () {
+    var new_token = 'test';
+    while (token_database[new_token] != undefined) {
+        new_token = uuid();
+    }
+    token_database[new_token] = true;
+    return new_token;
+}
