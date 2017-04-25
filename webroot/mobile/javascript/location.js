1bf48b5aadca337674d77984f0a40150fe8b041a
Chudi Huang <t-chhuan@microsoft.com>
2017-04-25 17:32:17 +0800

    Alpha of mobile page

---
+++ webroot/mobile/javascript/location.js
@@ -0,0 +1,25 @@
+﻿var display;
+
+$(function () {
+    display = $('.location');
+    $('.update').click(updateLocation);
+
+    updateLocation();
+});
+
+function updateLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(watchCallback, watchErr);
+    } else {
+        alert('Location service not supported by your browser');
+    }
+}
+
+function watchCallback(position) {
+    console.log(JSON.stringify(position));
+    display.html(JSON.stringify(position));
+}
+
+function watchErr(err) {
+    display.html(JSON.stringify(err));
+}
