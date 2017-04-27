b980c2888c01fac75ce8597787601c8fe3ae185e
HDYA-BackFire <hdya_backfire@sina.cn>
2017-04-27 15:56:57 +0800

    admin service

---
+++ admin.js
@@ -0,0 +1,24 @@
+﻿const uuid = require('node-uuid');
+
+var admin_service_tokens = {};
+
+function generate_token() {
+    var new_token = uuid();
+    while (token_database[new_token] != undefined) {
+        new_token = uuid();
+    }
+    admin_service_tokens[new_token] = true;
+    return new_token;
+}
+
+module.export.examine_admin = function (uuid, pwd) {
+    if (uuid == 'admin_service' && pwd === 'admin_service') {
+        return generate_token();
+    } else {
+        return null;
+    }
+}
+
+module.export.examine_admin_token = function (token) {
+    return (admin_service_tokens[token] != undefined);
+}
--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var app = new express();
 
-var user_token = require('./user_token.js');
+var admin_service = require('admin_service.js');
+var user_token = require('user_token.js');
 var location_service = require('location.js');
 
 app.use('/mobile', express.static('webroot/mobile'));
@@ -25,12 +26,39 @@ app.post('/token', function (req, res) {
 });
 
 /* Location */
-app.post('position', function (req, res) {
+app.post('/position', function (req, res) {
     location_service.registe_location(req.params.token, {
         latitude: req.params.latitude,
         longitude: req.params.longitude
     })
 });
+
+/* admin_serviceistrator */
+app.post('/admin_service', function (req, res) {
+    var ret = admin_service.examine_admin(req.params.username, req.params.password);
+    if (ret == null) {
+        res.status(401).send({
+            error: 'invalid login info'
+        })
+    } else {
+        res.send({
+            token: ret
+        })
+    }
+})
+
+app.get('/position', function (req, res) {
+    var ret = admin_service.examine_admin_token(req.params.token);
+    if (ret == null) {
+        res.status(401).send({
+            error: 'invalid admin_service token'
+        })
+    } else {
+        res.send({
+            positions: location_service.get_active_visitors()
+        })
+    }
+})
 
 app.listen(8080, function () {
     console.log('Server started on port 8080');
