9c98cb36b58417b86aea1649d96a6d0ffe14999d
HDYA-BackFire <hdya_backfire@sina.cn>
2017-06-05 15:40:19 +0800

    add three.js for visualization page

--- webroot/visualization/javascript/location_demo.js
+++ webroot/visualization/javascript/location_demo.js
@@ -1,9 +1,11 @@
 ﻿var display, map;
 var token;
 
+var camera;
+var visitors = {};
+
 $(function () {
     display = $('.display');
-    display = $('.map');
 
     display.html('Logging in');
     $.ajax({
@@ -24,10 +26,52 @@ $(function () {
         }
     });
 
-    map.width(window.innerWidth);
-    map.height(window.innerHeight);
+    var scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+
+    camera.position.z = 50;
+
+    var render = function () {
+        requestAnimationFrame(render);
+
+        renderer.render(scene, camera);
+    };
+
+    render();
 });
+
+function converter(position) {
+    position.latitude *= 60 * 1852;
+    position.longitude *= 60 * 1852 * Math.cos(position.latitude / 180 * 3.141592654);
+}
+
+function addVisitor(uuid, position) {
+    if (visitors[uuid] != undefined) {
+        return;
+    }
+    converter(position);
+    var geometry = new THREE.BoxGeometry(position.longitude, position.latitude, position.altitude);
+    var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    var cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+
+    visitors[uuid] = cube;
+}
+
+function moveVisitor(uuid, position) {
+    if (visitors[uuid] == undefined) {
+        return;
+    } else {
+        converter(position);
+        visitors[uuid].position.x = position.longitude;
+        visitors[uuid].position.y = position.latitude;
+        visitors[uuid].position.z = position.altitude;
+    }
+}
 
 function startQuery() {
     setTimeout(function () {
@@ -39,12 +83,32 @@ function startQuery() {
             },
             success: function (data) {
                 var dis = '';
+                var count = 0;
+                var longitudeCenter = 0.0;
+                var latitudeCenter = 0.0;
+                
                 for (var uuid in data.positions) {
                     dis = dis.concat(uuid).concat('<br>')
                         .concat('\t').concat(JSON.stringify(data.positions[uuid].position)).concat('<br>')
                         .concat('\t').concat(data.positions[uuid].timestamp).concat('<br>')
                         .concat('<br>');
+                    
+                    if (visitors[uuid] == undefined) {
+                        addVisitor(uuid, data.positions[uuid].position);
+                    } else {
+                        moveVisitor(uuid, data.positions[uuid].position);
+                    }
+
+                    count++;
+                    longitudeCenter += data.positions[uuid].position.longitude;
+                    latitudeCenter += data.positions[uuid].position.latitude;
                 }
+
+                longitudeCenter /= count;
+                latitudeCenter /= count;
+                camera.position.x = longitudeCenter;
+                camera.position.y = latitudeCenter;
+
                 console.log(JSON.stringify(data));
                 display.html(dis);
             },
