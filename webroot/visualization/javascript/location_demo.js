89b4c7232dbcd44422aa5db1da164b49a5ddde06
HDYA-BackFire <hdya_backfire@sina.cn>
2017-06-05 17:23:11 +0800

    refine visualization page, add helper elements

--- webroot/visualization/javascript/location_demo.js
+++ webroot/visualization/javascript/location_demo.js
@@ -1,8 +1,11 @@
 ﻿var display, map;
 var token;
 
+var cameraHeight = 50;
+
 var scene, camera;
 var visitors = {};
+var positionStartPoint;
 
 $(function () {
     display = $('.display');
@@ -29,11 +32,37 @@ $(function () {
     scene = new THREE.Scene();
     camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
+    camera.position.set(0, cameraHeight, 0);
+    camera.lookAt(new THREE.Vector3(0, 0, 0));
+
+    var groundGeometry = new THREE.PlaneGeometry(1200, 1200, 1, 1);
+    ground = new THREE.Mesh(groundGeometry, new THREE.MeshLambertMaterial({
+        color: 0x000000
+    }));
+    ground.position.y = -10;
+    ground.rotation.x = - Math.PI / 2;
+    ground.receiveShadow = true;
+    scene.add(ground);
+
+    var axisHelper = new THREE.AxisHelper(800);
+    scene.add(axisHelper);
+
+    var gridHelper = new THREE.GridHelper(800, 50);
+    gridHelper.position = new THREE.Vector3(0, 0, 0);
+    gridHelper.rotation = new THREE.Euler(0, 0, 0);
+    scene.add(gridHelper);
+
+    var gridHelper2 = gridHelper.clone();
+    gridHelper2.rotation = new THREE.Euler(Math.PI / 2, 0, 0);
+    scene.add(gridHelper2);
+
+    var gridHelper3 = gridHelper.clone();
+    gridHelper3.rotation = new THREE.Euler(Math.PI / 2, 0, Math.PI / 2);
+    scene.add(gridHelper3);
+
     var renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
-
-    camera.position.z = 50;
 
     var render = function () {
         requestAnimationFrame(render);
@@ -45,16 +74,13 @@ $(function () {
 });
 
 function converter(position) {
+    var latitude = position.latitude;
     position.latitude *= 60 * 1852;
-    position.longitude *= 60 * 1852 * Math.cos(position.latitude / 180 * 3.141592654);
+    position.longitude *= 60 * 1852 * Math.cos(latitude / 180 * Math.PI);
 }
 
 function getRandomColor() {
-    return '0x' +
-        (function (color) {
-            return (color += '' + (Math.floor(Math.random() * 255)).toString(16))
-                && (color.length == 6) ? color : arguments.callee(color);
-        })('');
+    return '0x' + ("00000" + ((Math.random() * 16777215 + 0.5) >> 0).toString(16)).slice(-6);
 }
 
 function addVisitor(uuid, position) {
@@ -62,8 +88,8 @@ function addVisitor(uuid, position) {
         return;
     }
     converter(position);
-    var geometry = new THREE.BoxGeometry(position.longitude, position.latitude, position.altitude);
-    var material = new THREE.MeshBasicMaterial({ color: getRandomColor() });
+    var geometry = new THREE.BoxGeometry(1, 1, 1);
+    var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
     var cube = new THREE.Mesh(geometry, material);
     scene.add(cube);
 
@@ -75,14 +101,21 @@ function moveVisitor(uuid, position) {
         return;
     } else {
         converter(position);
-        visitors[uuid].position.x = position.longitude;
-        visitors[uuid].position.y = position.latitude;
-        visitors[uuid].position.z = position.altitude;
+        console.log('visitor position:', JSON.stringify(position));
+
+        if (positionStartPoint == undefined) {
+            positionStartPoint = {
+                longitude: position.longitude,
+                latitude: position.latitude,
+            }
+        }
+
+        visitors[uuid].position.set(position.longitude - positionStartPoint.longitude, position.altitude, position.latitude - positionStartPoint.latitude);
     }
 }
 
 function startQuery() {
-    setTimeout(function () {
+    setInterval(function () {
         $.ajax({
             url: '/position',
             method: 'GET',
@@ -102,22 +135,31 @@ function startQuery() {
                         .concat('<br>');
 
                     if (visitors[uuid] == undefined) {
-                        addVisitor(uuid, data.positions[uuid].position);
-                    } else {
-                        moveVisitor(uuid, data.positions[uuid].position);
+                        addVisitor(uuid, {
+                            longitude: data.positions[uuid].position.longitude,
+                            latitude: data.positions[uuid].position.latitude,
+                            altitude: data.positions[uuid].position.altitude
+                        });
                     }
+                    moveVisitor(uuid, {
+                        longitude: data.positions[uuid].position.longitude,
+                        latitude: data.positions[uuid].position.latitude,
+                        altitude: data.positions[uuid].position.altitude
+                    });
 
                     count++;
                     longitudeCenter += data.positions[uuid].position.longitude;
                     latitudeCenter += data.positions[uuid].position.latitude;
                 }
 
+                if (count == 0) {
+                    return;
+                }
+
                 longitudeCenter /= count;
                 latitudeCenter /= count;
-                camera.position.x = longitudeCenter;
-                camera.position.y = latitudeCenter;
 
-                console.log(JSON.stringify(data));
+                console.log('data received:', JSON.stringify(data));
                 display.html(dis);
             },
             fail: function (err) {
